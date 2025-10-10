ffd1c19e5d07d2f5eacab29524ea5e5309bf2833
surjanthakur <tsurjan506@gmail.com>
2025-10-11 01:20:54 +0530

    save

--- contextTodo/src/App.jsx
+++ contextTodo/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   };
 
   const deleteTodo = (id) => {
-    setTodos((prev) => prev.filter(prev.id != id));
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
   };
 
   const toggleComplete = (id) => {
