a3e4a65c428f544c6f32f4f0cbb8419cdb800abc
surjanthakur <tsurjan506@gmail.com>
2025-10-11 00:27:26 +0530

    save

--- contextTodo/src/components/todoform.jsx
+++ contextTodo/src/components/todoform.jsx
@@ -1,14 +1,23 @@
 import { useState } from "react";
-import UseTodo from "../context/todoContext";
+import { UseTodo } from "../context/todoContext";
 
 export function TodoForm() {
   const [todo, setTodo] = useState("");
+  const { addTodo } = UseTodo();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo({ todo, completed: false });
+    setTodo("");
+  };
   return (
-    <form className="flex">
+    <form onSubmit={handleSubmit} className="flex">
       <input
         type="text"
         placeholder="Write Todo..."
         className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
       />
       <button
         type="submit"
