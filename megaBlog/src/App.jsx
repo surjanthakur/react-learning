7f68e1d70d9680cafb85cbf0f1a5ca8114e0a66e
surjanthakur <tsurjan506@gmail.com>
2025-10-12 00:53:59 +0530

    save

--- megaBlog/src/App.jsx
+++ megaBlog/src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
 
 function App() {
-  const appurl = import.meta.env.VITE_APPWRITEURL;
+  const appurl = import.meta.env.VITE_APPWRITE_URL;
+  console.log(appurl);
   return <></>;
 }
 
