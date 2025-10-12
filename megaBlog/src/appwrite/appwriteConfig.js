19b823ca3335d7cc2abc5bd2ade672a29c911b7e
surjanthakur <tsurjan506@gmail.com>
2025-10-12 19:57:54 +0530

    save

--- megaBlog/src/appwrite/appwriteConfig.js
+++ megaBlog/src/appwrite/appwriteConfig.js
@@ -1,7 +1,16 @@
 import config from "../config/config";
 import { Client, Account, ID, Databases, Query } from "appwrite";
 
-export class Services {}
+export class Services {
+  client = new Client();
+  database;
+  bucket;
+  constructor() {
+    this.client
+      .setEndpoint(config.appWriteurl)
+      .setProject(config.appWriteProjectid);
+  }
+}
 
 const services = new Services();
 
