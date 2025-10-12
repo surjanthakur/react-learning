3cd3cfc5a2d9629a739f5f560ea281236e115ce6
surjanthakur <tsurjan506@gmail.com>
2025-10-12 19:36:54 +0530

    save

--- megaBlog/src/appwrite/appwriteConfig.js
+++ megaBlog/src/appwrite/appwriteConfig.js
@@ -1,2 +1,8 @@
 import config from "../config/config";
 import { Client, Account, ID, Databases, Query } from "appwrite";
+
+export class Services {}
+
+const services = new Services();
+
+export default services;
