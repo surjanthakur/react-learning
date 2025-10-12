a6a504fddecef4c810dd931d251d16553dd7fb46
surjanthakur <tsurjan506@gmail.com>
2025-10-13 00:51:04 +0530

    save

--- megaBlog/src/appwrite/appwriteConfig.js
+++ megaBlog/src/appwrite/appwriteConfig.js
@@ -1,19 +1,45 @@
 import config from "../config/config";
-import { Client, Account, ID, TablesDB, Storage, Query } from "appwrite";
+import { Client, Account, ID, Storage, Query, Databases } from "appwrite";
 
 export class Services {
   client = new Client();
-  database;
+  databases;
   bucket;
   constructor() {
     this.client
       .setEndpoint(config.appWriteurl)
       .setProject(config.appWriteProjectid);
-    this.database = TablesDB(this.client);
-    this.bucket = Storage(this.client);
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
+
+  async createPost({ title, content, image, status, userId }) {
+    try {
+      return await this.databases.createPost(ID.unique(), {
+        title,
+        content,
+        image,
+        status,
+        userId,
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async updatePost(slug, { title, content, image, status }) {
+    try {
+      return await this.databases.updatePost(slug, {
+        title,
+        content,
+        image,
+        status,
+      });
+    } catch (error) {
+      throw error;
+    }
   }
 }
 
 const services = new Services();
-
 export default services;
