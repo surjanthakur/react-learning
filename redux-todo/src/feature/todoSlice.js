34ef2466463aa32e37e5df30978622e8918c5c8d
surjanthakur <tsurjan506@gmail.com>
2025-10-16 17:53:35 +0530

    save

--- redux-todo/src/feature/todoSlice.js
+++ redux-todo/src/feature/todoSlice.js
@@ -1,1 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [{ id: nanoid(), task: "learn about redux" }],
+};
+
+const todoSlices = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.todos.push({ id: nanoid(), task: action.payload });
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, removeTodo } = todoSlices.actions;
+export default todoSlices.reducer;
