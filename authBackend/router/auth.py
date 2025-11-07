f165391af5fed02da4c9d9640255b77d680e34f5
surjanthakur <tsurjan506@gmail.com>
2025-11-07 20:51:42 +0530

    save

--- authBackend/router/auth.py
+++ authBackend/router/auth.py
@@ -4,16 +4,37 @@ from dbModels import User
 from database import get_session_db
 from pwdlib import PasswordHash
 from pydantic_schema import showSignup
+from uuid import uuid4
 
 router = APIRouter(tags=["authentication"])
 
 password_hash = PasswordHash.recommended()
 
 
+# hash plain password
+def hashPassword(password):
+    return password_hash.hash(password)
+
+
 # signup user
 @router.post("/signup")
 def signup_user(signupform: showSignup, session_db: Session = Depends(get_session_db)):
-    pass
+    user = session_db.query(User).filter(User.email == signupform.email)  # type: ignore
+    if user is not None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="user is already exist please try again ❌",
+        )
+    new_user = User(
+        id=str(uuid4),
+        username=signupform.username,
+        email=signupform.email,
+        password=hashPassword(signupform.password),
+    )
+    session_db.add(new_user)
+    session_db.commit()
+    session_db.refresh(new_user)
+    return {"message": "signup user successfully"}
 
 
 # login user
