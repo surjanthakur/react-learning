d53fb20ac768279ca9ef1bf2d635e442624e1253
surjanthakur <tsurjan506@gmail.com>
2025-11-11 00:46:14 +0530

    save

--- authBackend/router/listings.py
+++ authBackend/router/listings.py
@@ -2,12 +2,16 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlmodel import Session
 from database import get_session_db
 from dbModels import Tweet
+from authFunction import get_current_user
 
 router = APIRouter(tags=["tweets"])
 
 
 @router.get("/tweet")
-def get_all_tweets(sesssion_db: Session = Depends(get_session_db)):
+def get_all_tweets(
+    sesssion_db: Session = Depends(get_session_db),
+    curr_user: Session = Depends(get_current_user),
+):
     all_tweets = sesssion_db.query(Tweet).all()
     if all_tweets is not None:
         return all_tweets
