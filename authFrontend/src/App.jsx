c941379728f66e21a5812b23bc9ddea4afe0364e
surjanthakur <tsurjan506@gmail.com>
2025-11-10 09:49:58 +0530

    save

--- authFrontend/src/App.jsx
+++ authFrontend/src/App.jsx
@@ -2,16 +2,18 @@ import "./App.css";
 import Signup from "./components/signupForm";
 import Login from "./components/loginForm";
 import Navbar from "./nav";
-import { Router, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Router>
-        <Route path="/signup" element={Signup}></Route>
-        <Route path="/login" element={Login}></Route>
-      </Router>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
