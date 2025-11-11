2bfb6fa8bcf37cfb2d70b797ce88f4af75a650f0
surjanthakur <tsurjan506@gmail.com>
2025-11-11 16:35:44 +0530

    save

---
+++ learn-redux/src/features/todo.js
@@ -0,0 +1,38 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [{ id: nanoid, task: "get up 6am", completed: false }],
+};
+
+export const TodoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    // add new todo
+    addTodo: (state, action) => {
+      const newTodo = {
+        id: nanoid(),
+        task: action.payload,
+        completed: false,
+      };
+
+      state.todos.push(newTodo);
+    },
+    // update todo
+    updateTodo: (state, action) => {
+      const { id, task } = action.payload;
+      const editTodo = state.todos.find((todo) => todo.id == id);
+      if (editTodo) {
+        editTodo.task = task;
+      }
+    },
+    // remove todo
+    removeTodo: (state, action) => {
+      const { id } = action.payload;
+      const deleteTodo = state.todos.find((todo) => todo.id == id);
+      if (deleteTodo) {
+        state.todos.filter((todo) => todo.id != id);
+      }
+    },
+  },
+});
