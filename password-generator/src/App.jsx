2e697da823270616ae912d27371e4e4b206d30ed
surjanThakur <surjanthakur@surjanThakurs-MacBook-Air.local>
2025-11-15 08:09:45 +0530

    savae

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -1,45 +1,72 @@
-import './App.css'
-import { useState ,useCallback , useEffect } from 'react'
+import { useCallback, useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [charLength , setCharLength] = useState(5)
-  const [number , setNumber] = useState(false)
-  const [specialChar , setSpecialChar] =useState(false)
-  const [password , setPassword] = useState("")
+  const [charLength, setCharLength] = useState(5);
+  const [number, setNumber] = useState(false);
+  const [specialChar, setSpecialChar] = useState(false);
+  const [password, setPassword] = useState('');
 
- const passgenerator =  useCallback( ()=> {
-    let pass = ""
-    let string= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    if (number) string += "1234567890"
-    if (specialChar) string += "!@#$%^&*()_+=<>?/|"
+  const passgenerator = useCallback(() => {
+    let pass = '';
+    let string = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (number) string += '1234567890';
+    if (specialChar) string += '!@#$%^&*()_+=<>?/|';
 
-    for(let i=1; i<=charLength ; i++ ){
-      let genPass = Math.floor(Math.random()*string.length+1)
-      pass +=string.charAt(genPass)
+    for (let i = 1; i <= charLength; i++) {
+      let genPass = Math.floor(Math.random() * string.length + 1);
+      pass += string.charAt(genPass);
     }
-    setPassword(pass)
-  } , [charLength , number , specialChar , setPassword])
+    setPassword(pass);
+  }, [charLength, number, specialChar, setPassword]);
 
-useEffect(()=> {
-  passgenerator()
-} ,[charLength , number , specialChar])
+  useEffect(() => {
+    passgenerator();
+  }, [charLength, number, specialChar]);
 
   return (
     <>
-   <div className="main">
-    <div className="pass-generator-area">
-      <input className='pass-form' type='text' value={password} readOnly/>
-      <button className='btn'>copy</button>
-    </div>
-    <div className="apply-features">
-      <input className='charlength' type='range' max={15} min={5} value={charLength} onChange={(e)=> {setCharLength(e.target.value)}}/>length &nbsp;
-      <input className='numbers' type='checkbox' defaultChecked={number} onChange={()=> {setNumber((prev)=>!prev)}}/>numbers
-      <input className='character' type='checkbox' defaultChecked={specialChar} onChange={()=> {setSpecialChar((prev) => !prev)}}/>special-char
-    </div>
+      <div className="main">
+        <div className="pass-generator-area">
+          <input className="pass-form" type="text" value={password} readOnly />
+          <button className="btn">copy</button>
+        </div>
+        <div className="apply-features">
+          <label>length</label>
+          <input
+            className="charlength"
+            type="range"
+            max={25}
+            min={5}
+            value={charLength}
+            onChange={(e) => {
+              setCharLength(e.target.value);
+            }}
+          />
 
-   </div>
+          <label>numbers</label>
+          <input
+            className="numbers"
+            type="checkbox"
+            defaultChecked={number}
+            onChange={() => {
+              setNumber((prev) => !prev);
+            }}
+          />
+
+          <label>special-char</label>
+          <input
+            className="character"
+            type="checkbox"
+            defaultChecked={specialChar}
+            onChange={() => {
+              setSpecialChar((prev) => !prev);
+            }}
+          />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
