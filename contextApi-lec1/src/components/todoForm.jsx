ab55945bd2e580898a760710b35d36e3b7bb58bc
Surjanthakur <tsurjan506@gmail.com>
2025-11-19 20:15:57 +0530

    save

--- contextApi-lec1/src/components/todoForm.jsx
+++ contextApi-lec1/src/components/todoForm.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react';
 import { useTodo } from '../context/todoContext';
 
 export default function TodoForm() {
-  const [task, setTask] = useState('');
+  const [todo, setTodo] = useState('');
 
   const { addTodo } = useTodo();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    addTodo(task);
-    setTask('');
+    addTodo(todo);
+    setTodo('');
   };
   return (
     <form className="flex" onSubmit={handleSubmit}>
@@ -17,7 +17,7 @@ export default function TodoForm() {
         type="text"
         value={task}
         onChange={(e) => {
-          setTask(e.target.value);
+          setTodo(e.target.value);
         }}
         placeholder="Write Todo..."
         className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
