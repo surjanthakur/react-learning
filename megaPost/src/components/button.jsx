447a00a9c0f5a9e4011204a09d023c981410f905
Surjanthakur <tsurjan506@gmail.com>
2025-12-06 18:15:36 +0530

    save

---
+++ megaPost/src/components/button.jsx
@@ -0,0 +1,18 @@
+export default function button({
+  children,
+  type = 'button',
+  bgColor = 'bg-blue-600',
+  textColor = 'text-white',
+  className = '',
+  ...props
+}) {
+  return (
+    <button
+      type={type}
+      className={`px-4 py-2  rounded-4xl ${bgColor} ${textColor}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
