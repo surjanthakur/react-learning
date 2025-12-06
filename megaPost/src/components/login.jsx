81ce6a42cc086dcd3cb5bf2d0948c750ef553853
Surjanthakur <tsurjan506@gmail.com>
2025-12-06 22:41:58 +0530

    save

--- megaPost/src/components/login.jsx
+++ megaPost/src/components/login.jsx
@@ -5,13 +5,72 @@ import Button from './button';
 import Input from './input';
 import { useDispatcht } from 'react-redux';
 import authservice from '../appwrite/auth';
+import toast, { Toaster } from 'react-hot-toast';
+import { useState } from 'react';
 
 export default function Login() {
   const dispatch = useDispatcht();
   const navigate = useNavigate();
+  const { register, handleSubmit } = useForm();
+  const [error, setError] = useState('');
+
+  const login = async (data) => {
+    setError('');
+    try {
+      const session = await authservice.login(data);
+      if (session) {
+        const user = await authservice.getCurrentUser();
+        if (user) dispatch(authLogin(user));
+        navigate('/');
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
   retun(
-    <div>
-      <h1></h1>
+    <div className="flex items-center justify-center w-full">
+      <div
+        className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
+      >
+        <div className="mb-2 flex justify-center">
+          <span className="inline-block w-full max-w-[100px]">
+            <h1>logo</h1>
+          </span>
+        </div>
+        <h2 className="text-center text-2xl font-bold leading-tight">
+          Sign in to your account
+        </h2>
+        <p className="mt-2 text-center text-base text-black/60">
+          Don&apos;t have any account?&nbsp;
+          <Link
+            to="/signup"
+            className="font-medium text-primary transition-all duration-200 hover:underline"
+          >
+            Sign Up
+          </Link>
+        </p>
+        {/* display error */}
+        {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+        <form>
+          <div>
+            {/* email */}
+            <input
+              label="Email: "
+              placeholder="Enter your email"
+              type="email"
+            />
+            {/* password */}
+            <input
+              label="Password: "
+              type="password"
+              placeholder="Enter your password"
+            />
+            <Button type="submit" className="w-full">
+              Sign in
+            </Button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
