4666685a4f765647d8633139c0051e260e5118b4
Surjanthakur <tsurjan506@gmail.com>
2025-12-06 23:23:00 +0530

    save

--- megaPost/src/components/signup.jsx
+++ megaPost/src/components/signup.jsx
@@ -1,5 +1,65 @@
-import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+import { login as authLogin } from '../store/authSlice';
+import Button from './button';
+import Input from './input';
+import { useDispatcht } from 'react-redux';
+import authservice from '../appwrite/auth';
+import toast, { Toaster } from 'react-hot-toast';
+import { useState } from 'react';
 
 export default function Signup() {
-  return <div></div>;
+  return (
+    <div className="flex items-center justify-center w-full">
+      <div
+        className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
+      >
+        <div className="mb-2 flex justify-center">
+          <span className="inline-block w-full max-w-[100px]">
+            <h1>logo</h1>
+          </span>
+        </div>
+        <h2 className="text-center text-2xl font-bold leading-tight">
+          create your account
+        </h2>
+        <p className="mt-2 text-center text-base text-black/60">
+          have any account?&nbsp;
+          <Link
+            to="/login"
+            className="font-medium text-primary transition-all duration-200 hover:underline"
+          >
+            Sign in
+          </Link>
+        </p>
+        {/* display error */}
+        {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+
+        <form onSubmit={handleSubmit(create)}>
+          <div className="space-y-5">
+            {/* name */}
+            <Input
+              label="Full Name: "
+              type="text"
+              placeholder="Enter your full name"
+            />
+            {/* email */}
+            <Input
+              label="Email: "
+              placeholder="Enter your email"
+              type="email"
+            />
+            {/* password */}
+            <Input
+              label="Password: "
+              type="password"
+              placeholder="Enter your password"
+            />
+            <Button type="submit" className="w-full">
+              Create Account
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
