70d8cb28f8cf205d3171b348af537bfb6128f72b
Surjanthakur <tsurjan506@gmail.com>
2025-12-06 11:18:40 +0530

    save

--- megaPost/src/appwrite/config.js
+++ megaPost/src/appwrite/config.js
@@ -14,8 +14,70 @@ export class DatabseServices {
     this.databases = new Databases(this.client);
     this.storage = new Storage(this.client);
   }
+
+  async createPost({ title, slug, content, image, status, userId }) {
+    try {
+      return await this.databases.createDocument({
+        databaseId: ConfigEnv.appwrite_database_Id,
+        collectionId: ConfigEnv.appwrite_table_Id,
+        documentId: slug,
+        data: {
+          title,
+          content,
+          image,
+          status,
+          userId,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async updatePost(slug, { title, content, image, status }) {
+    try {
+      return await this.databases.updateDocument({
+        databaseId: ConfigEnv.appwrite_database_Id,
+        collectionId: ConfigEnv.appwrite_table_Id,
+        documentId: slug,
+        data: {
+          title,
+          slug,
+          content,
+          image,
+          status,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async getpost(slug) {
+    try {
+      return await this.databases.getDocument({
+        databaseId: ConfigEnv.appwrite_database_Id,
+        collectionId: ConfigEnv.appwrite_table_Id,
+        documentId: slug,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async deletePost(slug) {
+    try {
+      await this.databases.deleteDocument({
+        databaseId: ConfigEnv.appwrite_database_Id,
+        collectionId: ConfigEnv.appwrite_table_Id,
+        documentId: slug,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
 
-const services = new DatabseServices();
+const services = new DatabseServices({});
 
 export default services;
