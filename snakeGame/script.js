fec104279e4d80260f38287bfec59732cefa4c50
Surjanthakur <tsurjan506@gmail.com>
2025-12-07 18:14:31 +0530

    save

--- snakeGame/script.js
+++ snakeGame/script.js
@@ -1,14 +1,14 @@
-const board = document.querySelector('.board');
-const blockHight = 30;
-const blockWidth = 30;
+document.addEventListener('DOMContentLoaded', () => {
+  const board = document.querySelector('.board');
+  const blockHight = 30;
+  const blockWidth = 30;
 
-// create columns and rows accourding to width and hight of screen
-const col = Math.floor(board.clientWidth / blockWidth);
-const rows = Math.floor(board.clientHeight / blockHight);
+  const col = Math.floor(board.clientWidth / blockWidth);
+  const rows = Math.floor(board.clientHeight / blockHight);
 
-// create blocks  based on col*rows
-for (let i = 0; i < col * rows; i++) {
-  const block = document.createElement('div');
-  block.classList.add('block');
-  board.appendChild(block);
-}
+  for (let i = 0; i < col * rows; i++) {
+    const block = document.createElement('div');
+    block.classList.add('block');
+    board.appendChild(block);
+  }
+});
