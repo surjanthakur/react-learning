18c253fde2e7d645bf2a4780c5e9afb616684845
Surjanthakur <tsurjan506@gmail.com>
2025-12-07 18:52:32 +0530

    save

--- snakeGame/script.js
+++ snakeGame/script.js
@@ -3,12 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const blockHight = 30;
   const blockWidth = 30;
 
-  const col = Math.floor(board.clientWidth / blockWidth);
+  const cols = Math.floor(board.clientWidth / blockWidth);
   const rows = Math.floor(board.clientHeight / blockHight);
 
-  for (let i = 0; i < col * rows; i++) {
-    const block = document.createElement('div');
-    block.classList.add('block');
-    board.appendChild(block);
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      const block = document.createElement('div');
+      block.classList.add('block');
+      board.appendChild(block);
+    }
   }
 });
