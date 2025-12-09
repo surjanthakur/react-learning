fd2f579d7ff2f7f567605108ee417a73f7a4e18e
Surjanthakur <tsurjan506@gmail.com>
2025-12-09 11:13:14 +0530

    save

---
+++ megaPost/src/components/authLayout.jsx
@@ -0,0 +1,5 @@
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+export default function Protected() {}
--- snakeGame/script.js
+++ snakeGame/script.js
@@ -1,16 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
   const board = document.querySelector('.board');
-  const blockHight = 30;
-  const blockWidth = 30;
+  const blockHight = 50;
+  const blockWidth = 50;
 
   const cols = Math.floor(board.clientWidth / blockWidth);
   const rows = Math.floor(board.clientHeight / blockHight);
+
+  const blocks = [];
 
   for (let row = 0; row < rows; row++) {
     for (let col = 0; col < cols; col++) {
       const block = document.createElement('div');
       block.classList.add('block');
       board.appendChild(block);
+      blocks[`${row}-${col}`] = block;
     }
   }
+
+  const snake = [
+    { x: 1, y: 20 },
+    { x: 1, y: 21 },
+    { x: 1, y: 22 },
+  ];
+
+  function snakeRender() {
+    snake.forEach((el) => {
+      blocks[`${el.x}-${el.y}`].classList.add('snakeColor');
+    });
+  }
+
+  let directions = 'left';
+
+  setInterval(() => {
+    let head = null;
+    if (directions === 'left') {
+      head = { x: snake[0].x, y: snake[0].y - 1 };
+    }
+
+    snake.forEach((el) => {
+      blocks[`${el.x}-${el.y}`].classList.remove('snakeColor');
+    });
+
+    snake.unshift(head);
+    snake.pop();
+    snakeRender();
+  }, 400);
 });
