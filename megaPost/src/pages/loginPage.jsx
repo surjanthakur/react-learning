ca45919c34a2fb93cd74f643ce1a273eea599871
Surjanthakur <tsurjan506@gmail.com>
2025-12-09 16:02:42 +0530

    save

---
+++ megaPost/src/pages/loginPage.jsx
@@ -0,0 +1,9 @@
+import Login from "../components/login";
+
+export default function loginPage() {
+  return (
+    <div className="py-8">
+      <Login />
+    </div>
+  );
+}
---
+++ megaPost/src/pages/signupPage.jsx
@@ -0,0 +1,9 @@
+import Signup from "../components/signup";
+
+export default function signupPage() {
+  return (
+    <div className="py-8">
+      <Signup />
+    </div>
+  );
+}
