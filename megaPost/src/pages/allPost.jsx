404f09f5c06aa133eb824def1d212cded234aa94
Surjanthakur <tsurjan506@gmail.com>
2025-12-09 16:19:02 +0530

    save

--- megaPost/src/pages/allPost.jsx
+++ megaPost/src/pages/allPost.jsx
@@ -1,4 +1,7 @@
 import PostCard from "../components/postCard";
+import Container from "../container/container";
+import services from "../appwrite/config";
+import { useState, useEffect } from "react";
 
 export default function AllPost() {
   return <div></div>;
