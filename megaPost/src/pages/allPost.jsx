547f5ebbfdae13fb782896364b7bec9063f6b830
Surjanthakur <tsurjan506@gmail.com>
2025-12-09 16:33:00 +0530

    save

--- megaPost/src/pages/allPost.jsx
+++ megaPost/src/pages/allPost.jsx
@@ -16,5 +16,17 @@ export default function AllPost() {
     };
     fetchPost();
   });
-  return <div></div>;
+  return (
+    <div className="w-full py-8">
+      <Container>
+        <div className="flex flex-wrap">
+          {allPost.map((post) => (
+            <div key={post.$id} className="p-2 w-1/4">
+              <PostCard post={post} />
+            </div>
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
 }
