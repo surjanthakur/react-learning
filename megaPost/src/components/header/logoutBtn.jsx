03fbc66236c5740757ab4a868bdcbe5cce444405
Surjanthakur <tsurjan506@gmail.com>
2025-12-11 18:06:06 +0530

    svae

--- megaPost/src/components/header/logoutBtn.jsx
+++ megaPost/src/components/header/logoutBtn.jsx
@@ -1,25 +1,24 @@
-import { useDispatch } from 'react-redux';
-import authservice from '../../appwrite/auth';
-import { logout } from '../../store/authSlice';
-
-const handlelogoutchange = () => {
-  const dispatch = useDispatch();
-  authservice
-    .logout()
-    .then(() => {
-      dispatch(logout());
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-};
+import { useDispatch } from "react-redux";
+import authservice from "../../appwrite/auth";
+import { logout } from "../../appwrite/auth";
 
 function LogoutBtn() {
+  const dispatch = useDispatch();
+  const logoutHandle = () => {
+    authservice
+      .logout()
+      .then(() => {
+        dispatch(logout());
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
   return (
     <button
       type="button"
       className="inline-block bg-gray-700 text-white px-6 py-2 duration-200 hover:bg-red-500 rounded-4xl"
-      onClick={handlelogoutchange}
+      onClick={logoutHandle}
     >
       log out
     </button>
