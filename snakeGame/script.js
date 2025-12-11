e73f2a318591b1d92d14718bd387b7b6520a16e6
Surjanthakur <tsurjan506@gmail.com>
2025-12-11 13:54:39 +0530

    save

--- snakeGame/script.js
+++ snakeGame/script.js
@@ -51,9 +51,9 @@ document.addEventListener('DOMContentLoaded', () => {
   setInterval(() => {
     let head = null;
     if (directions === 'right') {
-      head = { x: snake[0].x, y: snake[0].y - 1 };
-    } else if (directions === 'left') {
       head = { x: snake[0].x, y: snake[0].y + 1 };
+    } else if (directions === 'left') {
+      head = { x: snake[0].x, y: snake[0].y - 1 };
     } else if (directions === 'up') {
       head = { x: snake[0].x - 1, y: snake[0].y };
     } else if (directions == 'down') {
