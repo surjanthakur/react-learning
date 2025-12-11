2a9a8c0e79ec947c15f5008fc3a062b5d7d8b553
Surjanthakur <tsurjan506@gmail.com>
2025-12-11 13:50:16 +0530

    save

--- snakeGame/script.js
+++ snakeGame/script.js
@@ -26,11 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   //*in this snake array we have snake with block location based on  x and y axis.
-  const snake = [
-    { x: 1, y: 20 },
-    { x: 1, y: 21 },
-    { x: 1, y: 22 },
-  ];
+  const snake = [{ x: 1, y: 20 }];
 
   // *in this function we render the snake array.
   function snakeRender() {
@@ -39,25 +35,36 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  let directions = 'left';
+  let directions = 'right';
+  addEventListener('keydown', (e) => {
+    if (e.key == 'ArrowUp') {
+      directions = 'up';
+    } else if (e.key == 'ArrowDown') {
+      directions = 'down';
+    } else if (e.key == 'ArrowRight') {
+      directions = 'right';
+    } else if (e.key == 'ArrowLeft') {
+      directions = 'left';
+    }
+  });
 
-  // *render snake position every  3 seconds.
   setInterval(() => {
     let head = null;
-    if (directions === 'left') {
+    if (directions === 'right') {
       head = { x: snake[0].x, y: snake[0].y - 1 };
+    } else if (directions === 'left') {
+      head = { x: snake[0].x, y: snake[0].y + 1 };
+    } else if (directions === 'up') {
+      head = { x: snake[0].x - 1, y: snake[0].y };
+    } else if (directions == 'down') {
+      head = { x: snake[0].x + 1, y: snake[0].y };
     }
 
-    snake.forEach((el) => {
-      blocks[`${el.x}-${el.y}`].classList.remove('snakeColor');
+    snake.forEach((block) => {
+      blocks[`${block.x}-${block.y}`].classList.remove('snakeColor');
     });
-
     snake.unshift(head);
     snake.pop();
     snakeRender();
   }, 300);
-});
-
-addEventListener('keydown', (e) => {
-  console.log(e.key);
 });
